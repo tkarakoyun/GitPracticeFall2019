0f2a24d7d1658de177ab29e91d7faa7e2806ffc7
tkarakoyun <tubakarakoyun@hotmail.com>
2020-03-03 19:54:01 -0500

    commit message

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,5 +4,9 @@ public class StringPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        String str = " Java if Fun";
+        System.out.println(str);
+        String str1 = "Tugba";
+        System.out.println(str1);
     }
 }
